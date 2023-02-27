cff451c457aba459f433589744829a556860728a
Abhishek Anuj <26abhishekanuj@gmail.com>
2023-02-27 18:41:47 +0530

    [Abhishek] Add. counter to popup

--- myscripts.js
+++ myscripts.js
@@ -11,23 +11,33 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 function doSomething(){
+
+    var buttonCount =0;
     var buttonList = document.querySelectorAll( "li.reusable-search__result-container div.entity-result__actions > div > button.ember-view:enabled:not(.artdeco-button--muted)");
     for(var i = 0;i<buttonList.length;i++){
         (function(index) {
             setTimeout(()=>{
                 console.log("sending request");
                 if(buttonList[index].innerText == "Connect"){
-                buttonList[index].click();
+                // buttonList[index].click();
                 setTimeout(()=>{
                     console.log("sending request again with popup");
                     var sendButton = document.querySelector("div.send-invite button.artdeco-button--primary");
-                    sendButton?.click();
+                    // sendButton?.click();
+                    buttonCount++;
+                    chrome.runtime.sendMessage({message:buttonCount});
                 },1000);
             }
             },index*2000)
         })(i);
    }
 }
+
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    document.getElementById("countSent").innerText = message.message;
+   console.log(message.message);
+  });
+  
 
 
 
