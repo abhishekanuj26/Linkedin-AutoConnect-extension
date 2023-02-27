3a6c9f00eda9a59ef474f1ea878cccd7a25fe6b8
Abhishek Anuj <26abhishekanuj@gmail.com>
2023-02-27 19:32:53 +0530

    [Abhishek] Added style to popup page

--- myscripts.js
+++ myscripts.js
@@ -4,39 +4,38 @@ document.addEventListener('DOMContentLoaded', function() {
 
         chrome.scripting.executeScript({
             target: {tabId:tab.id},
-            function: doSomething
+            function: sendRequest
         });
     });
   });
 
 
-function doSomething(){
-
+function sendRequest(){
     var buttonCount =0;
     var buttonList = document.querySelectorAll( "li.reusable-search__result-container div.entity-result__actions > div > button.ember-view:enabled:not(.artdeco-button--muted)");
     for(var i = 0;i<buttonList.length;i++){
         (function(index) {
             setTimeout(()=>{
                 console.log("sending request");
-                if(buttonList[index].innerText == "Connect"){
-                // buttonList[index].click();
+                if(buttonList[index].innerText === "Connect"){
+                buttonList[index].click();
                 setTimeout(()=>{
                     console.log("sending request again with popup");
                     var sendButton = document.querySelector("div.send-invite button.artdeco-button--primary");
-                    // sendButton?.click();
+                    sendButton?.click();
                     buttonCount++;
                     chrome.runtime.sendMessage({message:buttonCount});
-                },1000);
+                },500);
             }
             },index*2000)
         })(i);
    }
 }
 
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function(message) {
     document.getElementById("countSent").innerText = message.message;
    console.log(message.message);
-  });
+});
   
 
 
